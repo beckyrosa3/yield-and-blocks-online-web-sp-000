8683a80702eacf98d27932c620d766e8796d1139
Becky Rosa <becky_m_rosa@yahoo.com>
2021-02-21 22:27:33 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -5,9 +5,12 @@ def hello_t
     yield array[i]
     i = i + 1
   end
-
+    array
+  end
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
+  else
+    puts "Hey! No block was given!"
   end
 end
